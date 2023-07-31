3bfff2d7e47b86f78a53fcbecccbc1e14f197aae
Stanislav <kaggy8@ya.ru>
2023-07-31 20:22:44 +0300

    Initial Commit

---
+++ Weather_App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather_App
+//
+//  Created by Stanislav Demyanov on 31.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
